ab3305f9159e9519170a65d756c860df89eb2922
Anastasia Boyko <taboyko24@gmail.com>
2021-03-24 01:21:35 +0200

    профиль соц сети и секция статистики

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js - главный компонент (обертка) создаваемого приложения;
+// Компоненты обязательно называются с большой буквы. В одном файл - прописываем работу только одного компонента;
+// компонент - это функция, куда мы закидываем настроийки, а на выходе получаем Virtual DOM, который потом рендерится в настоящий DOM.
+
+import React from "react"; //обзятельно import React  в каждый файл, чтобы использовать JSX и он мог  скомпилироваться и транспайлиться
+
+// Components
+import Profile from "./components/Profile"; // 1. Profile
+import Statistics from "./components/Statistics"; //2. Statistics
+
+import userData from "./user.json";
+import statisticalData from "./statistical-data.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>homework-01</h1>
+      {/* 1. рендерим и вставляем часть кода */}
+      <Profile
+        name={userData.name}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+
+import s from "./Profile.module.css";
+
+const Profile = ({ name, tag, location, avatar, stats }) => {
+  const { followers, views, likes } = stats;
+  return (
+    <div className="section">
+      <div className={s.description}>
+        <img src={avatar} alt={name} width="150" className={s.avatar} />
+        <p className={s.name}>{name}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
+
+      <ul className={s.stats}>
+        <li>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}> {followers}</span>
+        </li>
+
+        <li>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}> {views}</span>
+        </li>
+
+        <li>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}> {likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+// Можно добавить`isRequired` к любому приведённому выше типу,
+// чтобы показывать предупреждение,
+// если проп не передан
+
+Profile.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
+export default Profile;
